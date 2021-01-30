9f66b583c72844544d03bcca79c59bb406e98a76
Adonka <schoolemailern29@gmail.com>
2021-01-30 00:46:17 -0500

    Updated  Task1.py
    Printed as the folling instruction in the file.

--- Task1.py
+++ Task1.py
@@ -11,7 +11,32 @@ with open('calls.csv', 'r') as f:
     reader = csv.reader(f)
     calls = list(reader)
 
+#Dictionary can check if the number already exits or not. Then we can count by the size of dictionary.
+num_dic = {}
+count = 0
+#Numbers are shown in the first two columns(j), so for-loop can go over the first two elments in two lists (each list = i)
+for i in range(len(texts)):
+    j = 0
+    while(j < 2):
+        for number in texts[i][j]:
+            if number not in num_dic:
+                num_dic[number] = 1
+            else:
+                num_dic[number] += 1
+            j += 1
+for i in range(len(calls)):
+    j = 0
+    while(j < 2):
+        for number in calls[i][j]:
+            if number not in num_dic:
+                num_dic[number] = 1
+            else:
+                num_dic[number] += 1
+            j += 1
+#asigned the size of the dictionary
+count = len(num_dic)
 
+print("There are {} different telephone numbers in the records.".format(count))
 """
 TASK 1:
 How many different telephone numbers are there in the records? 
